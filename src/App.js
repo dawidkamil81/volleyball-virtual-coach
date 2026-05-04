cdec828a21a5d13ce75e96fb7156bc6f1fb7ae7a
Oliwier Wojtowicz <255733@edu.p.lodz.pl>
2026-05-04 15:08:02 +0200

    Dodanie podstrony tymczasowej do testowania testSpeech czy działa syntezator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Training from './components/Training'
 import Stats from './components/Stats';
+import TestSpeech from './components/TestSpeech';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Dashboard />} />
       <Route path="/trening" element={<Training />} />
       <Route path="/stats" element={<Stats />} />
+      <Route path="/testSpech" element={<TestSpeech />} />
     </Routes>
   );
 }
