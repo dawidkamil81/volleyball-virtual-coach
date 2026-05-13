77923d7a5144d0ac904e97f3785a01d1b707bd5f
dawidkamil81 <hubert2004s@wp.pl>
2026-05-13 21:11:06 +0200

    podstawowa konfiguracja websocketa

---
+++ backend/schemas.py
@@ -0,0 +1,25 @@
+
+from pydantic import BaseModel, field_validator
+
+
+class Landmark(BaseModel):
+    #konkretny punkt na ciele
+
+    x: float
+    y: float
+    z: float
+    visibility: float
+
+
+class PoseData(BaseModel):
+    #media pipe wymaga 33 punktow dla klatki
+
+    landmarks: list[Landmark]
+
+    @field_validator("landmarks")
+    @classmethod
+    def exactly_33_landmarks(cls, value: list[Landmark]) -> list[Landmark]:
+        if len(value) != 33:
+            msg = "Landmarks musi zawierać dokładnie 33 elementy"
+            raise ValueError(msg)
+        return value
